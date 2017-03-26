ac0c5b0517fd8913fb1516f5ab9644011fd5ab66
Peter McGill <petermcgill94@gmail.com>
2017-03-26 19:54:33 +0100

    adding CART analysis code

---
+++ CART/CART.py
@@ -0,0 +1,222 @@
+# CART on the Bank Note dataset
+from random import seed
+from random import randrange
+from csv import reader
+
+
+# Load a CSV file
+def load_csv(filename):
+	file = open(filename, "r")
+	lines = reader(file)
+	dataset = list(lines)
+	return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+
+# Calculate WAIS pulser selection efficieny
+def efficiency(actual, predicted):
+	correct = 0
+	total = 0
+	for i in range(len(actual)):
+		if actual[i] == 1:
+			total += 1
+			if predicted[i] == 1:
+				correct += 1
+	return correct / float(total) * 100.0
+
+# calculate the WAIS pulser selection purity
+def purity(actual, predicted):
+	incorrect = 0
+	total = 0
+	for i in range(len(actual)):
+		if predicted[i] == 1:
+			total += 1
+			if actual[i] == 0:
+				incorrect += 1
+	return (1 - incorrect / float(total)) * 100.0
+	
+			
+			
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+	for fold in folds:
+		train_set = list(folds)
+		train_set.remove(fold)
+		train_set = sum(train_set, [])
+		test_set = list()
+		for row in fold:
+			row_copy = list(row)
+			test_set.append(row_copy)
+			row_copy[-1] = None
+		predicted = algorithm(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		scores.append(accuracy)
+	return scores
+
+# Split a dataset based on an attribute and an attribute value
+def test_split(index, value, dataset):
+	left, right = list(), list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
+
+# Calculate the Gini index for a split dataset
+def gini_index(groups, class_values):
+	gini = 0.0
+	for class_value in class_values:
+		for group in groups:
+			size = len(group)
+			if size == 0:
+				continue
+			proportion = [row[-1] for row in group].count(class_value) / float(size)
+			gini += (proportion * (1.0 - proportion))
+	return gini
+
+# Select the best split point for a dataset
+def get_split(dataset):
+	class_values = list(set(row[-1] for row in dataset))
+	b_index, b_value, b_score, b_groups = 999, 999, 999, None
+	for index in range(len(dataset[0])-1):
+		for row in dataset:
+			groups = test_split(index, row[index], dataset)
+			gini = gini_index(groups, class_values)
+			if gini < b_score:
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
+
+# Create a terminal node value
+def to_terminal(group):
+	outcomes = [row[-1] for row in group]
+	return max(set(outcomes), key=outcomes.count)
+
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, depth):
+	left, right = node['groups']
+	del(node['groups'])
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right)
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right)
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left)
+	else:
+		node['left'] = get_split(left)
+		split(node['left'], max_depth, min_size, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right)
+	else:
+		node['right'] = get_split(right)
+		split(node['right'], max_depth, min_size, depth+1)
+
+# Build a decision tree
+def build_tree(train, max_depth, min_size):
+	root = get_split(train)
+	split(root, max_depth, min_size, 1)
+	return root
+
+# Make a prediction with a decision tree
+def predict(node, row):
+	if row[node['index']] < node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
+
+# Classification and Regression Tree Algorithm
+def decision_tree(train, test, max_depth, min_size):
+	print("Building tree from training data...")
+	tree = build_tree(train, max_depth, min_size)
+	print('Tree built:')
+	print_tree(tree)
+ 
+	print("Evaluating accuracy against testing data...")
+	predictions = list()
+	actual = list()
+	for row in test:
+		prediction = predict(tree, row)
+		predictions.append(prediction)
+		actual.append(row[-1])
+	print('Accuracy: %.2f',accuracy_metric(actual, predictions))
+	print('Efficency: %.2f',efficiency(actual,predictions))
+	print('Purity: %.2f', purity(actual,predictions))
+
+# Print a decision tree
+def print_tree(node, depth=0):
+	if isinstance(node, dict):
+		print('%s[X%d < %.3f]' % ((depth*' ', (node['index']+1), node['value'])))
+		print_tree(node['left'], depth+1)
+		print_tree(node['right'], depth+1)
+	else:
+		print('%s[%s]' % ((depth*' ', node)))
+
+
+print('Starting')
+
+# Test CART on Bank Note dataset
+# load and prepare data
+train = 'data/TrainingSet.csv'
+dataset = load_csv(train)
+
+test = 'data/TestingSet.csv'
+testingSet = load_csv(test)
+
+
+print('loaded data')
+
+# convert string attributes to integers
+for i in range(len(dataset[0])):
+	str_column_to_float(dataset, i)
+
+for i in range(len(testingSet[0])):
+	str_column_to_float(testingSet, i)
+
+
+max_depth = 3
+min_size = 100
+
+print('stripped data')
+
+decision_tree(dataset, testingSet, max_depth,min_size);
+
+
